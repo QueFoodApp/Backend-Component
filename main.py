cf8c44d6d86fd274d31bc0f4a93f6997643abdd8
Chen2939 <90723077+Chen2939@users.noreply.github.com>
2024-10-18 01:05:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import test, dbop
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+# routers/test -> /api/test
+app.include_router(test.router, prefix="/api")
+# routers/test -> /api/dbop
+app.include_router(dbop.router, prefix="/api")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "this is the base app file"}
