523e84357c6cd397ec83578542c5bdc29510ae3c
Haotian Yan <hyan227@uwo.ca>
2024-10-28 16:31:21 -0400

    First Push for Update authenticate

--- utils/db_authenticate.py
+++ utils/db_authenticate.py
@@ -12,14 +12,29 @@ DB_NAME = os.getenv("DATABASE")
 DB_USER = os.getenv("USER")
 DB_PASSWORD = os.getenv("PASSWORD")
 
+print(f"Connecting to database with the following settings:")
+print(f"Host: {DB_HOST}")
+print(f"Database: {DB_NAME}")
+print(f"User: {DB_USER}")
+print(f"Password: {DB_PASSWORD}")  # Be careful: this should not be printed in production
+
 
 def get_db():
-    connection = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT
+    if not DB_USER == "developuser":
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user="developuser",
+            password=DB_PASSWORD,
+            port=DB_PORT
+    )
+    else:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
     )
     cursor = connection.cursor()
     try:
